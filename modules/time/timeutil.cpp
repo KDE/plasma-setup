164eaf0fb7dc056600e33cd131b2b92aeb2bf636
Kristen McWilliam <kristen@kde.org>
2025-10-31 11:08:17 -0400

    feat: add working timezone setting functionality
    Transitions from the previous incomplete & broken timezone
    implementation to a fully functional one that makes use of the existing
    QML timezone picker from the clock KCM, while requiring far less
    custom code.

--- modules/time/timeutil.cpp
+++ modules/time/timeutil.cpp
@@ -1,4 +1,6 @@
 // SPDX-FileCopyrightText: 2023 by Devin Lin <devin@kde.org>
+// SPDX-FileCopyrightText: 2025 Kristen McWilliam <kristen@kde.org>
+//
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "timeutil.h"
@@ -6,48 +8,14 @@
 #include "timedate_interface.h"
 
 #include <QDBusPendingCallWatcher>
-#include <QDebug>
-#include <QRegularExpression>
 #include <QTimeZone>
 
-#include <KConfigGroup>
-#include <KSharedConfig>
-
 using namespace Qt::StringLiterals;
-
-static const QString format24hours = u"HH:mm:ss"_s;
-static const QString format12hours = u"HH:mm:ss ap"_s;
 
 TimeUtil::TimeUtil(QObject *parent)
     : QObject{parent}
     , m_dbusInterface(new OrgFreedesktopTimedate1Interface(u"org.freedesktop.timedate1"_s, u"/org/freedesktop/timedate1"_s, QDBusConnection::systemBus(), this))
-    , m_timeZoneModel{new TimeZoneModel{this}}
-    , m_filterModel{new TimeZoneFilterProxy{this}}
 {
-    m_filterModel->setSourceModel(m_timeZoneModel);
-
-    // retrieve is24HourTime
-    const auto config = KSharedConfig::openConfig(u"kdeglobals"_s, KConfig::SimpleConfig);
-    const auto group = KConfigGroup(config, "Locale"_L1);
-    m_is24HourTime = group.readEntry(u"TimeFormat"_s, format24hours) == format24hours;
-}
-
-bool TimeUtil::is24HourTime() const
-{
-    return m_is24HourTime;
-}
-
-void TimeUtil::setIs24HourTime(bool is24HourTime)
-{
-    if (is24HourTime != m_is24HourTime) {
-        auto config = KSharedConfig::openConfig("kdeglobals"_L1, KConfig::SimpleConfig);
-        auto group = KConfigGroup(config, "Locale"_L1);
-        group.writeEntry(u"TimeFormat"_s, is24HourTime ? format24hours : format12hours, KConfig::Notify);
-        config->sync();
-
-        m_is24HourTime = is24HourTime;
-        Q_EMIT is24HourTimeChanged();
-    }
 }
 
 QString TimeUtil::currentTimeZone() const
@@ -64,25 +32,6 @@ void TimeUtil::setCurrentTimeZone(const QString &timeZone)
             Q_EMIT currentTimeZoneChanged();
         }
     });
-}
-
-TimeZoneFilterProxy *TimeUtil::timeZones() const
-{
-    return m_filterModel;
-}
-
-QString TimeUtil::user() const
-{
-    return m_user;
-}
-
-void TimeUtil::setUser(const QString &user)
-{
-    if (m_user == user) {
-        return;
-    }
-    m_user = user;
-    Q_EMIT userChanged();
 }
 
 #include "moc_timeutil.cpp"
--- modules/time/timeutil.h
+++ modules/time/timeutil.h
@@ -1,13 +1,13 @@
 // SPDX-FileCopyrightText: 2023 by Devin Lin <devin@kde.org>
+// SPDX-FileCopyrightText: 2025 Kristen McWilliam <kristen@kde.org>
+//
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
 #include <QObject>
-#include <QProcess>
-#include <qqmlregistration.h>
 
-#include "timezonemodel.h"
+#include <qqmlregistration.h>
 
 class OrgFreedesktopTimedate1Interface;
 
@@ -17,38 +17,17 @@ class TimeUtil : public QObject
     QML_ELEMENT
     QML_SINGLETON
 
-    Q_PROPERTY(bool is24HourTime READ is24HourTime WRITE setIs24HourTime NOTIFY is24HourTimeChanged)
     Q_PROPERTY(QString currentTimeZone READ currentTimeZone WRITE setCurrentTimeZone NOTIFY currentTimeZoneChanged)
-    Q_PROPERTY(TimeZoneFilterProxy *timeZones READ timeZones CONSTANT)
-
-    /// This property holds the current user whose time information should be modified.
-    /// \note In case it is empty, assume this is the current user.
-    Q_PROPERTY(QString user READ user WRITE setUser NOTIFY userChanged)
 
 public:
     explicit TimeUtil(QObject *parent = nullptr);
 
-    [[nodiscard]] bool is24HourTime() const;
-    void setIs24HourTime(bool is24HourTime);
-
-    [[nodiscard]] QString user() const;
-    void setUser(const QString &user);
-
     QString currentTimeZone() const;
     void setCurrentTimeZone(const QString &timeZone);
 
-    TimeZoneFilterProxy *timeZones() const;
-
 Q_SIGNALS:
-    void is24HourTimeChanged();
     void currentTimeZoneChanged();
-    void userChanged();
 
 private:
-    bool m_is24HourTime;
-
     OrgFreedesktopTimedate1Interface *const m_dbusInterface;
-    QString m_user;
-    TimeZoneModel *const m_timeZoneModel;
-    TimeZoneFilterProxy *const m_filterModel;
 };
