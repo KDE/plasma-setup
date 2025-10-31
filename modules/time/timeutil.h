0cc66416ccc7c0f753aa85579d6aa3f523e39e8a
Kristen McWilliam <kristen@kde.org>
2025-10-31 11:08:34 -0400

    docs: add documentation for timezone methods

--- modules/time/timeutil.h
+++ modules/time/timeutil.h
@@ -11,23 +11,57 @@
 
 class OrgFreedesktopTimedate1Interface;
 
+/**
+ * Utility class for managing system timezone settings.
+ *
+ * TimeUtil provides a QML-accessible interface for reading and modifying
+ * the system timezone using the freedesktop.org timedate1 D-Bus interface.
+ */
 class TimeUtil : public QObject
 {
     Q_OBJECT
     QML_ELEMENT
     QML_SINGLETON
 
+    /**
+     * @property currentTimeZone
+     *
+     * The current system timezone identifier.
+     *
+     * This property holds the IANA timezone identifier (e.g., "America/New_York")
+     * for the system's current timezone.
+     */
     Q_PROPERTY(QString currentTimeZone READ currentTimeZone WRITE setCurrentTimeZone NOTIFY currentTimeZoneChanged)
 
 public:
     explicit TimeUtil(QObject *parent = nullptr);
 
+    /**
+     * Gets the current system timezone.
+     *
+     * @return The IANA timezone identifier as a QString.
+     */
     QString currentTimeZone() const;
+
+    /**
+     * Sets the system timezone.
+     *
+     * @param timeZone The IANA timezone identifier to set (e.g., "Europe/London").
+     */
     void setCurrentTimeZone(const QString &timeZone);
 
 Q_SIGNALS:
+    /**
+     * Emitted when the system timezone has been successfully changed.
+     */
     void currentTimeZoneChanged();
 
 private:
+    /**
+     * D-Bus interface for communicating with the freedesktop.org timedate1 service.
+     *
+     * This interface is used to query and modify system timezone settings
+     * through the org.freedesktop.timedate1 D-Bus service.
+     */
     OrgFreedesktopTimedate1Interface *const m_dbusInterface;
 };
